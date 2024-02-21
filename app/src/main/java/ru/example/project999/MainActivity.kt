956f61cb42716ddd6f135f5ce6df4e06041857f0
twinown <niyaz9797@gmail.com>
2024-02-22 00:01:12 +0300

    changes

--- app/src/main/java/ru/example/project999/MainActivity.kt
+++ app/src/main/java/ru/example/project999/MainActivity.kt
@@ -1,15 +1,17 @@
 package ru.example.project999
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     //смотри, тут та х -ня с аттачом, ты его не сделал ,потому будет ошибка
-    private val appName = application.getString(R.string.app_name)
+//    private val appName = application.getString(R.string.app_name)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("NN97", "thread: ${Thread.currentThread()}")
     }
 }
 //здесь ты держишь ссылку на активити вне активити.потому хип захломляется
