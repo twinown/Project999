ed6e4e440b5d5ce20b46c13bebf0d9677c7ba7cf
twinown <niyaz9797@gmail.com>
2024-03-28 16:45:41 +0300

    next

--- app/src/main/java/ru/example/project999/MainActivity.kt
+++ app/src/main/java/ru/example/project999/MainActivity.kt
@@ -10,62 +10,76 @@ import android.widget.TextView
 import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
-    //смотри, тут та х -ня с аттачом, активити с апликашнои не аттачилось ещё тут ,потому будет ошибка
+    //смотри, тут та х -ня с аттачом, активити с апликашном не аттачилось ещё тут ,потому будет ошибка
     //  private val appName = application.getString(R.string.app_name)
 
+    private val list = mutableListOf<Any>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        var count = 0
-
-        //закидываем месседж наверх очереди
-        //вызывая мэйнлуп - то есть ставим на очередь мэйн треда
-        //потому что если ты иниц вьюху свою в мэйн треде
-        //то можешь только в нём с ним и работать
-        textView.setOnClickListener {
-            Thread {
-                Handler(Looper.getMainLooper()).post {
-                    textView.setText("gogo")
-                }
-
-            }.start()
-        }
-
-        //та же хрень,что выше, но попроще
-        textView.setOnClickListener {
-            Thread {
-                runOnUiThread { textView.setText("gogo") }
-            }.start()
-        }
-
-        //кривой таймер
-        textView.setOnClickListener {
-            object : CountDownTimer(10000, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    textView.setText((millisUntilFinished/1000).toString())
-                }
-
-                override fun onFinish() {
-                    textView.append("\nfinished")
-                }
-            }.start()
-        }
-
-
-        //норм таймер
-        textView.setOnClickListener {
-            java.util.Timer().scheduleAtFixedRate(object :TimerTask(){
-                override fun run() = runOnUiThread {
-                   textView.setText((++count).toString())
-                }
-            },0,1000)
-        }
-
-
+       findViewById<TextView>(R.id.textView).setOnClickListener {
+           while (true)
+               list.add(A())
+       }
     }
+
+    data class A(private val l: Long = System.currentTimeMillis())
 }
+
+
+/* val textView = findViewById<TextView>(R.id.textView)
+ var count = 0*/
+
+//ANR
+/* textView.setOnClickListener {
+     Thread.sleep(50_000)
+     textView.setText("gogo")
+ }*/
+
+//закидываем месседж наверх очереди
+//вызывая мэйнлуп - то есть ставим на очередь мэйн треда
+//потому что если ты иниц вьюху свою в мэйн треде
+//то можешь только в нём с ней и работать
+/*  textView.setOnClickListener {
+      Thread {
+          Handler(Looper.getMainLooper()).post {
+              textView.setText("gogo")
+          }
+
+      }.start()
+  }*/
+
+//та же хрень,что выше, но попроще
+/*  textView.setOnClickListener {
+      Thread {
+          runOnUiThread { textView.setText("gogo") }
+      }.start()
+  }*/
+
+//кривой таймер
+/*  textView.setOnClickListener {
+      object : CountDownTimer(10000, 1000) {
+          override fun onTick(millisUntilFinished: Long) {
+              textView.setText((millisUntilFinished/1000).toString())
+          }
+
+          override fun onFinish() {
+              textView.append("\nfinished")
+          }
+      }.start()
+  }*/
+
+
+//норм таймер
+/*   textView.setOnClickListener {
+       java.util.Timer().scheduleAtFixedRate(object :TimerTask(){
+           override fun run() = runOnUiThread {
+              textView.setText((++count).toString())
+           }
+       },0,1000)
+   }*/
+
+
 //здесь ты держишь ссылку на активити вне активити.потому хип захломляется
 //object - синглтон - статичный объект
 //вообще,эту хрень мы не используем в андроиде
